25c1e65a402279e9ff4a420ef0e1b8397242d9c8
MUHIBULLAH <muhebollah.diu@gmail.com>
2023-03-20 22:59:54 +0600

    fet: collection over

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,3 +12,41 @@ export const menuLinks = [
     href: "#",
   },
 ];
+
+export const topColletions = [
+  {
+    title: "CryptoFunks",
+    price: "19,769.39",
+    percentage: "+26.52%",
+    image: "./images/top-collection1.jpg",
+    isSlected: true,
+  },
+  {
+    title: "Cryptix",
+    price: "2,769.39",
+    percentage: "+10.52%",
+    image: "./images/top-collection2.jpg",
+    isSlected: false,
+  },
+  {
+    title: "Frensware",
+    price: "9,232.39",
+    percentage: "+2.52%",
+    image: "./images/top-collection3.jpg",
+    isSlected: true,
+  },
+  {
+    title: "PunkArt",
+    price: "3,72.39",
+    percentage: "+1.5%",
+    image: "./images/top-collection4.jpg",
+    isSlected: false,
+  },
+  {
+    title: "Art Crypto",
+    price: "9,254.39",
+    percentage: "+3.10%",
+    image: "./images/top-collection3.jpg",
+    isSlected: true,
+  },
+];
